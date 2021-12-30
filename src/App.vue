3ccf75dda0ed54eef30c76a9f3329ecb5ec9f603
elmurie <elmurie@gmail.com>
2021-12-30 01:36:02 +0100

    refactor methods in appvue

--- src/App.vue
+++ src/App.vue
@@ -85,12 +85,6 @@ export default {
         this.isItDayOrNight = 'Night';
       }
     },
-    bgChooser() {
-      let d = new Date();
-      let hour = d.getHours;
-      console.log('ore', hour)
-      this.hourBg = hour;
-    },
     fetchWeather (e) {
       if (e.key == "Enter") {
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
@@ -119,7 +113,6 @@ export default {
   created() {
       let d = new Date();
       let hour = d.getHours();
-      console.log('ore', hour)
       this.hourBg = hour;
   }
 }
@@ -273,7 +266,7 @@ main {
 }
 .message h2 {
   color : #ffffff;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  text-shadow: 9px 4px 1px rgba(0, 0, 0, 0.25);
   font-size: 54px;
 }
 
